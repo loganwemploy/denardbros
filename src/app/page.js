bda7059f9b7d47b9fef198ec6751bf2e42f960c1
Loganwemploy <loganwemploy@gmail.com>
2025-03-13 16:30:28 -0500

    change the hero verbiage to 2.0  text

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get busy editing <code>src/app/page.js</code>.
+            Get busy living <code>or get busy dying</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
