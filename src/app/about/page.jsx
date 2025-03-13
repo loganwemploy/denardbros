8a94e03a0ec7cf908b5d0b68b18811842eac8a5a
luker369 <walker0369@gmail.com>
2025-03-13 16:50:44 -0500

    add useState and age variable to about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+"use client"
+import React,{ use, useState } from 'react'
 
 const About = () => {
+const [age, setAge] = useState(100)
   return (
-    <div>Yo, I'm livin!</div>
+    <div>Yo, I'm livin to {age}!</div>
   )
 }
 
