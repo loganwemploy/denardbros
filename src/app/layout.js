d2312a3d85b37c9eb8744ac8b62af0b3132b3207
Loganwemploy <loganwemploy@gmail.com>
2025-03-25 17:38:49 -0500

    add head and tailwind cdn link to layoutjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
