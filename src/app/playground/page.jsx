5c9c4904c6d2a036ca51569805e81e4a395f4cff
Loganwemploy <loganwemploy@gmail.com>
2025-05-06 01:41:09 -0500

    add playground text bg gray

--- src/app/playground/page.jsx
+++ src/app/playground/page.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ScrollSnapSlider from '../ScrollSnapSlider'
+import Hero from '../hero/Hero.js'
+import Section1 from '../section1/page'
+// import Section2 from '../section2/page'
+import Section2 from '../section2/page'
+
 
 export default function page() {
   return (
     <>
-    test
+    <Hero />
+    <Section1 />
+    <Section2 />
     {/* <ScrollSnapSlider /> */}
     </>
   )
