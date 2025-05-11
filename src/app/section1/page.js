d9f2894542c688eb9b549ec4d56f6003711a8711
luker369 <walker0369@gmail.com>
2025-05-10 21:09:12 -0500

    cleaned up layout

--- src/app/section1/page.js
+++ src/app/section1/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <section className="bg-gray-400 min-h-[60vh] flex justify-center p-8">
-      <div className="text-center max-w-2xl mx-auto">
+    <section className="bg-gray-400 w-full h-[70vh] md:h-[115vh] flex flex-grow justify-center p-8">
+      <div className="text-center relative pt-16 md:pt-[12rem] max-w-2xl mx-auto">
         <h2 className="text-3xl md:text-4xl font-semibold text-gray-800 leading-relaxed">
           Denard Bros is our competitive basketball camp full of inspiration and perspiration.
         </h2>
